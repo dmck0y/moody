613f4acba0ced94b1a0c1f2c017bf990b6283027
dmck0y <dmckoy85@gmail.com>
2018-12-12 07:32:00 -0500

    adding deps

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,6 +5,14 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
+                    loader: ["source-map-loader"]
+                },
+                enforce: "pre"
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
                     loader: "babel-loader"
                 }
             }
